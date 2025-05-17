f3f93a7b4104c6850f6fab89582113ada3267c5a
VishalRana2015 <69715143+VishalRana2015@users.noreply.github.com>
2025-05-17 15:16:20 +0530

    Practice

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,5 +1,58 @@
+const pets = [
+  { "id": "pet1",
+    "weightInKilograms" : "7.0"
+  }, 
+  {
+    "id": "pet2",
+    "weightInKilograms" :  null 
+  }
+]
+
+
+const books = [
+  {id : "book1",
+    name : "book1", 
+    authorId : [ "author1", "author2"]
+  },
+  {
+    id : "book2", 
+    name : "book2",
+    authorId: ["author2"]
+  }
+]
+
+const authorsMap = new Map();
+authorsMap.set("author1", {
+  id: "author1",
+  "name" : "author1"
+});
+
+authorsMap.set("author2", {
+  id: "author2",
+  name: "author2"
+});
+
+
 const resolvers = {
   Query: {
+    book : (_, args) => {
+      console.log("printing args from the book resolver function");
+      console.log(args);
+      try{
+        return books[args.index];
+      }catch(err){
+        console.log(err);
+        return null;
+      }
+    }, 
+    // fetch all books
+    books : () => {
+      return books;
+    },
+    // returns an array of all pets
+    pets : () => {
+      return pets;
+    },
     // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
@@ -14,6 +67,17 @@ const resolvers = {
     module: (_, { id }, { dataSources }) => {
       return dataSources.trackAPI.getModule(id);
     },
+  },
+  Book: {
+    authors: ( parent, args) => {
+      console.log("Printing args from the Book.authors resolver function");
+      console.log(args);
+      const authors = [];
+      parent.authorId.forEach(element => {
+        authors.push(authorsMap.get(element));
+      });
+      return authors;
+    }
   },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
